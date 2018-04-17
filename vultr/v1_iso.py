2a7b3770c673102f20699813405b5e9bff19378e
Leroy Hopson <git@leroy.geek.nz>
2018-04-18 02:13:53 +0700

    Add iso/create_from_url

--- vultr/v1_iso.py
+++ vultr/v1_iso.py
@@ -1,5 +1,5 @@
 '''Partial class to handle Vultr ISO API calls'''
-from .utils import VultrBase
+from .utils import VultrBase, update_params
 
 
 class VultrISO(VultrBase):
@@ -16,3 +16,18 @@ class VultrISO(VultrBase):
         '''
         params = params if params else dict()
         return self.request('/v1/iso/list', params, 'GET')
+
+    def create_from_url(self, url, params=None):
+        ''' /vi/iso/create_from_url
+        POST - account
+        Create a new ISO image on the current account.
+        The ISO image will be downloaded from a given URL.
+        Download status can be checked with the v1/iso/list call.
+
+        Link: https://www.vultr.com/api/#iso_create_from_url
+        '''
+        params = update_params(params, {
+            'url': url,
+        })
+        return self.request('/v1/iso/create_from_url', params, 'POST')
+
